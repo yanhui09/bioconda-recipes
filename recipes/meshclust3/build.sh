711e68d8297373d04a60d6d22a8b7954157fdef7
Yan Hui <me@yanh.org>
2023-09-06 14:55:15 +0200

    get prepared

--- recipes/meshclust3/build.sh
+++ recipes/meshclust3/build.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-
 mkdir build && cd build
-cmake .. \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX
-
+cmake ..
 make 
-ctest
-make install
+export PATH=$PWD:$PATH
